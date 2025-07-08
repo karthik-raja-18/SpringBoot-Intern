11c0c7c7bcf66c79160bc8ce4e93d80547f184f4
karthik-raja-18 <karthikraja.g2023cse@sece.ac.in>
2025-07-08 11:59:59 +0530

    day4 class work

---
+++ DAY-4/src/main/java/com/example/springbootfirst/Controllers/EmployeeController.java
@@ -0,0 +1,4 @@
+package com.example.springbootfirst.Controllers;
+
+public class EmployeeController {
+}
---
+++ DAY-4/src/main/java/com/example/springbootfirst/Services/EmployeeService.java
@@ -0,0 +1,4 @@
+package com.example.springbootfirst.Services;
+
+public class EmployeeService {
+}
