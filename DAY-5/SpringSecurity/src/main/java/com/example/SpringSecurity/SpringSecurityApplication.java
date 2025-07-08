131c6ac4b858d0506b9ec4ee4e92ddebcf3be4f6
karthik-raja-18 <karthikraja.g2023cse@sece.ac.in>
2025-07-08 22:42:38 +0530

    null

--- DAY-5/SpringSecurity/src/main/java/com/example/SpringSecurity/SpringSecurityApplication.java
+++ DAY-5/SpringSecurity/src/main/java/com/example/SpringSecurity/SpringSecurityApplication.java
@@ -1,11 +1,13 @@
-package com.example.springbootfirst;
+package com.example.SpringSecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootfirstApplication {
+public class SpringSecurityApplication {
+
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootfirstApplication.class, args);
+		SpringApplication.run(SpringSecurityApplication.class, args);
 	}
+
 }
--- DAY-5/SpringSecurity/src/main/java/com/example/SpringSecurity/repositories/EmployeeRepository.java
+++ DAY-5/SpringSecurity/src/main/java/com/example/SpringSecurity/repositories/EmployeeRepository.java
@@ -1,4 +1,7 @@
 package com.example.SpringSecurity.repositories;
 
-public class EmployeeRepository {
+import com.example.SpringSecurity.models.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
