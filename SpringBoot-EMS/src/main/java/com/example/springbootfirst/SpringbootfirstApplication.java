ec2c0c533bbe6aae5cab1ade53aecdcfb5a95b35
karthik-raja-18 <karthikraja.g2023cse@sece.ac.in>
2025-07-14 14:59:17 +0530

    Day-8 task

--- SpringBoot-EMS/src/main/java/com/example/springbootfirst/SpringbootfirstApplication.java
+++ SpringBoot-EMS/src/main/java/com/example/springbootfirst/SpringbootfirstApplication.java
@@ -1,5 +1,7 @@
 package com.example.springbootfirst;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- SpringBoot-EMS/src/main/java/com/example/springbootfirst/config/SpringConfiguration.java
+++ SpringBoot-EMS/src/main/java/com/example/springbootfirst/config/SpringConfiguration.java
@@ -1,45 +1,54 @@
 package com.example.springbootfirst.config;
 
+import com.example.springbootfirst.jwt.JwtAuthenticationFilter;
 import com.example.springbootfirst.services.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
 public class SpringConfiguration {
 
-  @Autowired
-  CustomUserDetailsService customUserDetailsService;
+    @Autowired
+    CustomUserDetailsService customUserDetailsService;
 
-  @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/api/auth/**").permitAll()
-                    .requestMatchers("/employee/**").permitAll() // ✅ Allow employee APIs publicly
-                    .anyRequest().authenticated()
-            )
-
-            .formLogin(form -> form
-                    .defaultSuccessUrl("/employee/", true)
-            )
-            .logout(logout -> logout.permitAll());
-
-    return http.build();
-  }
+    @Autowired
+    JwtAuthenticationFilter jwtAuthenticationFilter;
 
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
-  @Bean
-  public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-    return config.getAuthenticationManager();
-  }
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf((csrf)->csrf.disable())
+                .authorizeHttpRequests(auth-> {
+                    auth.requestMatchers("/api/auth/**").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration)
+            throws Exception {
+        return configuration.getAuthenticationManager();
+
+    }
+
+
 }
